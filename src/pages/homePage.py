b4f3699f9918ac942478f26bb92d43f0f6217830
aimer <2144547707@qq.com>
2025-08-12 00:09:45 +0800

    feat(homePage): 添加首页兴趣标签和帖子相关操作
    - 新增点击兴趣标签和帖子的功能
    - 增加点赞和获取点赞数的方法
    - 优化了页面元素的初始化和访问方式

--- src/pages/homePage.py
+++ src/pages/homePage.py
@@ -8,14 +8,50 @@ class HomePage():
         "search":'//*[@content-desc="搜索"]'
     }
     def __init__(self,d):
-        self.d = d
+        self.tabBar = d(className="android.view.ViewGroup",instance=0).child(className="androidx.recyclerview.widget.RecyclerView",instance=0)
+        self.postContainer=d(className="androidx.viewpager.widget.ViewPager",instance=2) \
+        .child(className="android.widget.FrameLayout").child(className="android.widget.FrameLayout") \
+        .child(className="android.view.ViewGroup").child(className="androidx.recyclerview.widget.RecyclerView")
+        
         for key,value in self.widgets.items():
             selector = d.xpath(value)
             if selector.wait(timeout=5):
                 setattr(self, key, selector.get())
             else:
                 setattr(self, key, None)  # 或者抛异常，提示元素没找到
+    def clickTabBar(self, instance=0):
+        """点击兴趣标签
+
+        Args:
+            instance (int, optional): 索引第几个标签. Defaults to 0.
+        """
+        self.tabBar.child(className="android.widget.FrameLayout",instance=instance).click()
         
+    def clickPost(self,instance=0):
+        """点击帖子
+
+        Args:
+            instance (int, optional): 索引第几个帖子. Defaults to 0.
+        """
+        self.postContainer.child(className="android.widget.FrameLayout",instance=instance*3).click()
+    def clickLike(self,instance=0):
+        """点击帖子点赞
+
+        Args:
+            instance (int, optional): 索引第几个帖子. Defaults to 0.
+        """
+        self.postContainer.child(className="android.widget.FrameLayout",instance=instance*3+1).child(className="android.widget.TextView",instance=1).click()
+    def getLikeNum(self,instance=0):
+        """获取帖子点赞数
+
+        Args:
+            instance (int, optional): 索引第几个帖子. Defaults to 0.
+
+        Returns:
+            int: 点赞数
+        """
+        return int(self.postContainer.child(className="android.widget.FrameLayout",instance=instance*3+1).child(className="android.widget.TextView",instance=1).get_text())
+    
     def slipMainContent(self):
         slip(self.d,self.mainContentXpath,1,0.3,1)
     def updateMainContent(self):
